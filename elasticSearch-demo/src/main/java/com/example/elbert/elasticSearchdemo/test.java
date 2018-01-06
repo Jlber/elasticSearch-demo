8cc4a26675cf35be37047d873bd49dca16ef8790
jiangenqing <15958134692@163.com>
2018-01-07 00:08:10 +0800

    提交下master1111

--- elasticSearch-demo/src/main/java/com/example/elbert/elasticSearchdemo/test.java
+++ elasticSearch-demo/src/main/java/com/example/elbert/elasticSearchdemo/test.java
@@ -4,7 +4,7 @@ package com.example.elbert.elasticSearchdemo;
  * Created by elbert on 2018/1/6.
  */
 public class test {
-    public static void main(String[] args) {
-        System.out.println("dddd");
+    void good(){
+
     }
 }
