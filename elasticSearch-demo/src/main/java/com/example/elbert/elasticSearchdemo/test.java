69bed725a135010689d478c5ae129c76e5510517
jiangenqing <15958134692@163.com>
2018-01-06 23:47:39 +0800

    第二次提交啊啊啊

--- elasticSearch-demo/src/main/java/com/example/elbert/elasticSearchdemo/test.java
+++ elasticSearch-demo/src/main/java/com/example/elbert/elasticSearchdemo/test.java
@@ -4,4 +4,11 @@ package com.example.elbert.elasticSearchdemo;
  * Created by elbert on 2018/1/6.
  */
 public class test {
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("asdasdsafsa");
+        System.out.println("dddd");
+        return super.equals(obj);
+
+    }
 }
